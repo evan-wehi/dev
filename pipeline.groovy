b2039162ec2426ad18160b7b27491478f911357c
Stuart Lee <lee.s@wehi.edu.au>
2016-03-11 16:02:04 +1100

    updates ready for testing

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,28 +1,22 @@
 ////////////////////////////////////////////////////////////
-// GATK-based variant-calling pipeline, WGS version.
+// GATK-based 'best' practices variant-calling pipeline.
 // 
 // This pipeline is a port of the VLSCI whole genome variant
-// calling pipeline from this Git repo to Bpipe:
+// calling pipeline  for from this Git repo to Bpipe:
 // 
 //     https://github.com/claresloggett/variant_calling_pipeline/
-//
-// This example is intended as an illustration of how a full pipeline
-// looks in Bpipe, especially for those who might be familiar with Ruffus.
-// There are a number of places where the pipeline is non-ideal, but it has
-// been kept that way to reflect the original as accurately as possible.
-//
-// One particular point of note is that the alignment output is stored
-// in SAM format by some of the intermediate stages which would be very
-// unadvisable for true whole-genome data. It is easy to make it store
-// output in BAM format instead, so this would be a very advisable 
-// change if you intend to run this pipeline on a lot of data. 
+// And modified from
+//     https://github.com/ssadedin/variant_calling_pipeline/
 //
 // There are a number of software requirements, which you should ensure are 
 // satisfied before running the pipeline. These need to be configured 
 // in the file called 'config.groovy' (which is loaded below). A template
-// is provided in config.groovy.template which you can use to 
+// is provided in config.groovy which you can use to 
 // create the file and set the right paths to your tools and reference
 // data.
+//
+// This pipeline attempts to recreate the WGS processing pipeline developed
+// by the Malaria Genomics Consortium for Plasmodium falciparum. 
 //
 // By default this pipeline will attempt to use all the available cores
 // on the computer it runs on. If you don't wish to do that, limit the 
@@ -33,7 +27,6 @@
 // Assumes: paired end reads
 // Assumes: files in form  *<sample_name>*_..._R1.fastq.gz, *<sample_name>*_..._R2.fastq.gz
 //
-// Author: Simon Sadedin, MCRI
 //
 ////////////////////////////////////////////////////////////
 
@@ -44,21 +37,32 @@ load 'config.groovy'
 load 'pipeline_stages_config.groovy'
 
 run {
-    // Align each pair of input files separately in parallel
-    "%_*_R*" * [
+    // Create index for reference file
+    [
+     createBWAIndex + fastaIndex
+    ] +
+    // Pre-process each unmapped BAM file separately in parallel
+    // Map, sort clean then dedup
+    // Realign indels
+    // Recalibrate base-quality scores
+    // Run QC
+    // Call variants
+    "*.bam" * [
+               remapBWAmem +
+               dedup +
+               realignIntervals +
+               realignIndels +
+               bqsrPass1 +
+               bqsrPass2 +
+               bqsrCheck +
+               bqsrApply +
                fastqc +
-               "%.gz" * [ alignBWA ]  +
-               alignToSamPE +
-               samToSortedBam + indexBam +
-               dedup + indexBam 
+               flagstat +
+               depthOfCoverage +
+               callVariants
+
     ] + 
-        // Merge all the bam files afterwards
-        mergeBams + 
-        indexBam + 
-    [ 
-       depthOfCoverage, 
-       realignIntervals + realign + indexBam +
-           baseQualRecalCount + baseQualRecalTabulate + indexBam  +
-               [ callIndels + filterIndels + annotateEnsembl, callSNPs + filterSNPs + annotateEnsembl ] 
-    ]
+        // For each g.vcf will jointly call genotypes then apply VQSR
+      [ genotype + vqsrGenerate + vqsrApply + annotateVariants + filterSNPs
+
 }
--- pipeline_stages_config.groovy
+++ pipeline_stages_config.groovy
@@ -12,14 +12,29 @@ createBWAIndex = {
     
 }
 
+fastaIndex = {
+    doc "index fasta file if not available"
+    exec "samtools faidx $REF"
+}
+createDictionary = {
+    doc "Create Sequence dictionary"
+    exec "java -jar $PICARD_HOME/CreateSequenceDictionary.jar R=$REF O=Pfalciparum.genome.dict"
+}
+
 remapBWAmem = {
     doc "Extract fastq from BAM file and remap using bwa mem"
     output.dir="$REFBASE/aligned_bams"
-    exec "sh revert_remamp.sh $input"
+    exec "sh revert_remamp.sh $input.bam"
 }
 
-realign = {
-    output.dir="align"
+realignIntervals = {
+    output.dir="aligned_bams"
+    exec """
+        java -Xmx4g -jar $GATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R $REF -I $input.bam -log $LOG -o $output.intervals
+    """
+}
+realignIndels = {
+    output.dir="aligned_bams"
     exec """
         java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar -T IndelRealigner -R $REF -I $input.bam -targetIntervals $input.intervals -log $LOG -o $output.bam
     """
@@ -28,7 +43,7 @@ realign = {
 dedup = {
     output.dir="align"
     exec """
-        java -Xmx6g -Djava.io.tmpdir=$TMPDIR -jar $PICARD_HOME/lib/MarkDuplicates.jar
+        java -Xmx6g -Djava.io.tmpdir=$TMPDIR -jar $PICARD_HOME/MarkDuplicates.jar
              INPUT=$input.bam 
              REMOVE_DUPLICATES=true 
              VALIDATION_STRINGENCY=LENIENT 
@@ -43,13 +58,7 @@ indexVCF = {
     exec "./vcftools_prepare.sh $input.vcf"
 }
 
-realignIntervals = {
-    // Hard-coded to take 2 known indels files right now
-    output.dir="align"
-    exec """
-        java -Xmx4g -jar $GATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R $REF -I $input.bam --known $GOLD_STANDARD_INDELS --known $INDELS_100G -log $LOG -o $output.intervals
-    """
-}
+
 
 bqsrPass1 = {
     doc "Recalibrate base qualities in a BAM file so that quality metrics match actual observed error rates"
@@ -109,13 +118,41 @@ bqsrApply = {
     """ 
 }
 
+fastqc = {
+    doc "Run FASTQC to generate QC metrics for the reads post-alignment"
+    output.dir = "qc"
+    transform('.bam')  to('_fastqc.zip')  {
+        exec "fastqc --quiet -o ${output.dir} -f bam_mapped $input.bam"
+    }
+}
 
+// QC metrics at BAM level
+flagstat = {
+    output.dir = "qc"
+    exec "samtools flagstat $input.bam > $output"
+}
+
+
+
+depthOfCoverage = {
+    output.dir="qc"
+    transform("bam") to ("sample_statistics","sample_interval_summary") {
+        exec """
+            java -Xmx4g -jar $GATK/GenomeAnalysisTK.jar 
+                    -T DepthOfCoverage 
+                    -R $REF -I $input.bam 
+                    -omitBaseOutput 
+                    -ct 1 -ct 5 -ct 10
+                    -o $output
+        """
+    }
 
 callVariants = {
     doc "Call SNPs/SNVs using GATK Haplotype Caller, produces .g.vcf"
     output.dir="variants"
+    transform(".bam") to (".g.vcf")
     exec """
-        java -Xmx2g -jar $GATK/GenomeAnalysisTK.jar
+        java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar
             -T HaplotypeCaller 
             -R $REF
             -I $input.bam
@@ -179,47 +216,53 @@ vqsrApply = {
             -o all_recalibrated.vcf
     """
 }
+
+// variant annotation using snpEff
+annotateVariants = {
+    doc "Annotate variants using snpEff"
+    exec """ 
+        java -Xmx8g -jar  $SNPEFF_HOME/snpEff.jar 
+            -no-downstream 
+            -no-upstream 
+            -v  
+            -c $configFile Pf3D7v3 all_recalibrated.vcf > all_recalibrated_anno.vcf
+    """
+}
+
+addRegionsAnnotation = {
+    doc "Include core regions from Pf genetic crosses"
+    exec """
+        bgzip $CORE_REGIONS
+        tabix -s 1 -b 2 -e 3 $CORE_REGIONS.gz
+        cat all_recalibrated_anno.vcf | vcf-annotate -a $CORE_REGIONS.gz \
+            -d key=INFO,ID=RegionType,Number=1,Type=String,Description='The type of genome region within which the variant is found. 
+            SubtelomericRepeat: repetitive regions at the ends of the chromosomes. 
+            SubtelomericHypervariable: subtelomeric region of poor conservation between the 3D7 reference genome and other samples. 
+            InternalHypervariable: chromosome-internal region of poor conservation between the 3D7 reference genome and other samples. 
+            Centromere: start and end coordinates of the centromere genome annotation. Core: everything else.' \
+            -c CHROM,FROM,TO,INFO/RegionType > all_recalibrated_anno_regions.vcf
+
+    """
+}
+
+// variant filtering
 @filter("filter")
 filterSNPs = {
     // Very minimal hard filters based on GATK recommendations. VQSR is preferable if possible.
     output.dir="variants"
 
     exec """
-        java -Xmx4g -jar $GATK/GenomeAnalysisTK.jar -T VariantFiltration 
-             -R $REF 
-             --filterExpression 'QD < 2.0 || MQ < 40.0 || FS > 60.0 || HaplotypeScore > 13.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0' 
-             --filterName 'GATK_MINIMAL_FILTER'
-             --variant $input.vcf 
-             -log $LOG 
-             -o $output.vcf
+        java -Xmx4g -jar $GATK/GenomeAnalysisTK.jar 
+            -T VariantFiltration 
+            -R $REF 
+            --filterExpression 'QD < 2.0 || MQ < 40.0 || FS > 60.0 || HaplotypeScore > 13.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0' 
+            --filterName 'GATK_MINIMAL_FILTER'
+            --filterExpression 'VQSLOD <= 0 or RegionType != "Core"'
+            --filterName 'MalariaGen Filter'
+            --variant all_recalibrated_anno_regions.vcf
+            -log $LOG 
+            -o all_recalibrated_anno_regions_filtered.vcf
     """
 }
 
 
-// QC metrics at BAM level
-flagstat = {
-    exec "samtools flagstat $input.bam > $output"
-}
-
-fastqc = {
-    doc "Run FASTQC to generate QC metrics for the reads"
-    output.dir = "fastqc"
-    transform('.fastq.gz')  to('_fastqc.zip')  {
-        exec "fastqc --quiet -o ${output.dir} $inputs.gz"
-    }
-}
-
-
-depthOfCoverage = {
-    output.dir="qc"
-    transform("bam") to ("sample_statistics","sample_interval_summary") {
-        exec """
-            java -Xmx4g -jar $GATK/GenomeAnalysisTK.jar 
-                    -T DepthOfCoverage 
-                    -R $REF -I $input.bam 
-                    -omitBaseOutput 
-                    -ct 1 -ct 5 -ct 10
-                    -o $output.prefix
-        """
-    }
-}
