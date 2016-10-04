69c227598f1879cf87801bc24ff4d701a801f95a
Evan Thomas <thomas.e@wehi.edu.au>
2016-10-04 16:08:13 +1100

    Convert to using the multiprocessing library rather than threading

--- BenchMarker/src/IOTask.py
+++ BenchMarker/src/IOTask.py
@@ -2,16 +2,17 @@
     Perform IO in a thread using dd
 """
 
-from threading import Thread
+from multiprocessing import Value
+from multiprocessing import Process
 from subprocess import Popen
 from timeit import default_timer
 from tempfile import mkstemp
 from os import remove
 
-class IOTask(Thread):
+class IOTask(Process):
     
     def __init__(self, size=10**9, blksize=10*7, dir=None, read=True, write=True):
-        Thread.__init__(self, target=self.go)
+        Process.__init__(self, target=self.go)
         self.size = size
         self.blksize = blksize
         self.read = read
@@ -19,15 +20,18 @@ class IOTask(Thread):
         
         self.fn = mkstemp(dir=dir)[1]
         
+        self.readTime = Value('d', 0.0)
+        self.writeTime = Value('d', 0.0)
+        
     def go(self):
         try:
             if self.write:
                 cmd = 'dd conv=fdatasync if=/dev/zero of=' + self.fn + ' count=' + str(int(round(self.size/self.blksize))) + ' bs=' + str(self.blksize)
-                self.writeTime = self.execute(cmd)
+                self.writeTime.value = self.execute(cmd)
             
             if self.read:
                 cmd = 'dd of=/dev/null if=' + self.fn + ' count=' + str(int(round(self.size/self.blksize))) + ' bs=' + str(self.blksize)
-                self.readTime = self.execute(cmd)
+                self.readTime.value = self.execute(cmd)
         finally:
             self.cleanup()
 
@@ -43,10 +47,10 @@ class IOTask(Thread):
             self.fn = None
     
     def readRate(self):
-        return self.rate(self.readTime)
+        return self.rate(self.readTime.value)
     
     def writeRate(self):
-        return self.rate(self.writeTime)
+        return self.rate(self.writeTime.value)
     
     def rate(self, dt):
         if dt == 0:
