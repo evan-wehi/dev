69f7e4155c3167a849a702065a61deab277c246d
Evan Thomas <thomas.e@wehi.edu.au>
2016-10-21 09:54:41 +1100

    add default values for arguments

--- BenchMarker/src/IO.py
+++ BenchMarker/src/IO.py
@@ -11,12 +11,13 @@ Created on 27Sep.,2016
 from IOTask import IOTask
 import argparse
 from dataunits import convertToString, convertToInt
+from os import getcwd
 
 parser = argparse.ArgumentParser('Benchmark IO performance with dd')
-parser.add_argument('--num_threads', type=int, help='The number of current IO threads')
-parser.add_argument('--dir',         type=str, help='The directory to create the temporary files in')
-parser.add_argument('--size',        type=str, help='The total size of each file')
-parser.add_argument('--blksize',     type=str, help='The block size to use')
+parser.add_argument('--num_threads', type=int, help='The number of current IO threads (defaults to 1).', default=1)
+parser.add_argument('--dir',         type=str, help='The directory to create the temporary files in (defaults to current directory).', default=getcwd())
+parser.add_argument('--size',        type=str, help='The total size of each file (defaults to 1GB)', default='1GB')
+parser.add_argument('--blksize',     type=str, help='The block size to use (defaults to 10MB)', default='10MB')
 
 args = parser.parse_args()
 
